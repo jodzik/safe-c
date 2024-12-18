5eea9e033fa2785cb20ff3b1b03da6f6993b08d0
kamenev <novble@mail.ru>
2024-12-18 21:33:47 +0700

    remove nokey specific, add already

--- safe_c.h
+++ safe_c.h
@@ -83,7 +83,7 @@ enum {
     ER_NOT_IMPL = -38,
     ER_OVERFLOW = -75,
     ER_NO_DATA = -61,
-    ER_CALIBRATION_FAIL = -200,
+    ER_ALREADY = -114,
     ER_1 = -1000,
     ER_2 = -2000,
     ER_3 = -3000,
